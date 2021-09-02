734eb70209740549117a4b06d51c23b3dfb6e69e
Himanshu pandey <42787064+Hpandey243@users.noreply.github.com>
2021-09-02 11:12:30 +0530

    Update RunningSum.java

--- Arrays/RunningSum.java
+++ Arrays/RunningSum.java
@@ -1,1 +1,11 @@
-
+//Problem : https://leetcode.com/problems/running-sum-of-1d-array/
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int i=1;
+        while(i<nums.length){
+            nums[i]+=nums[i-1];
+            i++;
+        }
+        return nums;        
+    }
+}
